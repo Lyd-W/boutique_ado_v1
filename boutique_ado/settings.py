1e1aa3f057c17e35ece00cf35d9d351b7d4b48f8
Lyd-W <209948061+Lyd-W@users.noreply.github.com>
2026-03-05 20:30:03 +0000

    Add basic checkout function

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 import os
 
+if os.path.isfile("env.py"):
+    import env
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -169,6 +172,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 FREE_DELIVERY_THRESHOLD = 50
 STANDARD_DELIVERY_PERCENTAGE = 10
+
+# Stripe settings
+STRIPE_CURRENCY = 'usd'
+STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY', '')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
