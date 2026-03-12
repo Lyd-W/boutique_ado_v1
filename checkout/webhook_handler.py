6b9f8b5dcfc03ce0bd332b35393c05c603f4fadb
Lyd-W <209948061+Lyd-W@users.noreply.github.com>
2026-03-12 16:56:35 +0000

    Fix unterminated f-string in webhook_handler

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -110,8 +110,8 @@ class StripeWH_Handler:
             self._send_confirmation_email(order)
             return HttpResponse(
                 content=(
-                    f'Webhook received: {
-                        event["type"]} | SUCCESS: Verified order already in database'
+                    f'Webhook received: {event["type"]}' 
+                    '| SUCCESS: Verified order already in database'
                 ),
                 status=200)
         else:
@@ -159,9 +159,8 @@ class StripeWH_Handler:
         self._send_confirmation_email(order)
         return HttpResponse(
             content=(
-                f'Webhook received: {
-                    event["type"]} | SUCCESS: Created order in webhook'
-            ),
+                f'Webhook received: {event["type"]} '
+                '| SUCCESS: Created order in webhook'),
             status=200)
 
     def handle_payment_intent_payment_failed(self, event):
